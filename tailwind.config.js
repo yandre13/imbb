7c747382d7d8642200b0c92a1648f628b2901e7a
yandre13 <yandre13.elsf@gmail.com>
2023-02-26 21:14:36 -0500

    feat: add prettier tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      tablet: '720px',
+      // => @media (min-width: 720px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      // desktopLarge: '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+    },
   },
   plugins: [],
 }
