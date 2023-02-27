fbc8ad7b96e825529920a959035824a0f599a5d8
yandre13 <yandre13.elsf@gmail.com>
2023-02-27 09:27:41 -0500

    fix: window is not defined error

--- src/app/head.tsx
+++
@@ -1,7 +0,0 @@
-export default function Head({ params }: { params: { slug: string } }) {
-  return (
-    <>
-      <title>My Page</title>
-    </>
-  )
-}
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -5,32 +5,40 @@ import { MdLightMode } from 'react-icons/md'
 import { MdDarkMode } from 'react-icons/md'
 
 function getInitialTheme() {
-  if (
-    localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-    // is dark mode
-    return 'dark'
-  } else {
-    // is light mode
-    return 'light'
+  if (typeof window !== 'undefined') {
+    if (
+      localStorage?.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      // is dark mode
+      return 'dark'
+    } else {
+      // is light mode
+      return 'light'
+    }
   }
 }
 
 function ThemeSwitcher() {
-  const [theme, setTheme] = useState(getInitialTheme)
+  const [theme, setTheme] = useState('')
 
   const toggleTheme = () =>
     theme === 'dark' ? setTheme('light') : setTheme('dark')
 
   useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
+    setTheme(getInitialTheme()!)
+  }, [])
+
+  useEffect(() => {
+    if (theme) {
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+      localStorage.theme = theme
     }
-    localStorage.theme = theme
   }, [theme])
 
   return (
